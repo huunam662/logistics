56dc3f9a4288ca75e0a700ebda21bcf914c5c271
Tô Minh Tuân <minhtuanto179@gmail.com>
2025-08-08 14:17:45 +0700

    Fix: sửa lại throw LogicErrException tránh lan truyền exception, thêm handler cho case throw raw message

--- src/main/java/warehouse_management/com/warehouse_management/service/InventoryItemService.java
+++ src/main/java/warehouse_management/com/warehouse_management/service/InventoryItemService.java
@@ -1,8 +1,11 @@
 package warehouse_management.com.warehouse_management.service;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import warehouse_management.com.warehouse_management.dto.Inventory.request.CreateInventoryItemReq;
+import warehouse_management.com.warehouse_management.mapper.InventoryItemMapper;
+import warehouse_management.com.warehouse_management.mapper.warehouse.WarehouseMapper;
 import warehouse_management.com.warehouse_management.model.InventoryItem;
 import warehouse_management.com.warehouse_management.repository.InventoryItemRepository;
 import warehouse_management.com.warehouse_management.utils.SimpleMapperUtil;
@@ -10,34 +13,13 @@ import warehouse_management.com.warehouse_management.utils.SimpleMapperUtil;
 @Service
 @RequiredArgsConstructor
 public class InventoryItemService {
+    private final InventoryItemMapper mapper;
     private final InventoryItemRepository inventoryItemRepository;
 
     public InventoryItem createInventory(CreateInventoryItemReq req) {
-        InventoryItem item = new InventoryItem();
-        SimpleMapperUtil.mapBasicFields(req, item);
-
-        // Map Specifications
-        if (req.getSpecifications() != null) {
-            InventoryItem.Specifications spec = new InventoryItem.Specifications();
-            SimpleMapperUtil.mapBasicFields(req.getSpecifications(), spec);
-            item.setSpecifications(spec);
-        }
-
-        // Map Pricing
-        if (req.getPricing() != null) {
-            InventoryItem.Pricing pricing = new InventoryItem.Pricing();
-            SimpleMapperUtil.mapBasicFields(req.getPricing(), pricing);
-            item.setPricing(pricing);
-        }
-
-        // Map Logistics
-        if (req.getLogistics() != null) {
-            InventoryItem.Logistics logistics = new InventoryItem.Logistics();
-            SimpleMapperUtil.mapBasicFields(req.getLogistics(), logistics);
-            item.setLogistics(logistics);
-        }
-
+        InventoryItem item = mapper.toInventoryItemModel(req);
         // Lưu DB
         return inventoryItemRepository.save(item);
+//        return null;
     }
 }
