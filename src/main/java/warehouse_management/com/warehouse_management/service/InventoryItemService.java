db66cc4a004a64595ca32fa92c1aa7957969e4f6
Tô Minh Tuân <minhtuanto179@gmail.com>
2025-08-08 14:21:35 +0700

    Fix: sửa lại throw LogicErrException tránh lan truyền exception, thêm handler cho case throw raw message

--- src/main/java/warehouse_management/com/warehouse_management/service/InventoryItemService.java
+++ src/main/java/warehouse_management/com/warehouse_management/service/InventoryItemService.java
@@ -1,14 +1,12 @@
 package warehouse_management.com.warehouse_management.service;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import warehouse_management.com.warehouse_management.dto.Inventory.request.CreateInventoryItemReq;
 import warehouse_management.com.warehouse_management.mapper.InventoryItemMapper;
-import warehouse_management.com.warehouse_management.mapper.warehouse.WarehouseMapper;
 import warehouse_management.com.warehouse_management.model.InventoryItem;
 import warehouse_management.com.warehouse_management.repository.InventoryItemRepository;
-import warehouse_management.com.warehouse_management.utils.SimpleMapperUtil;
+
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +14,7 @@ public class InventoryItemService {
     private final InventoryItemMapper mapper;
     private final InventoryItemRepository inventoryItemRepository;
 
-    public InventoryItem createInventory(CreateInventoryItemReq req) {
+    public InventoryItem createInventoryItem(CreateInventoryItemReq req) {
         InventoryItem item = mapper.toInventoryItemModel(req);
         // Lưu DB
         return inventoryItemRepository.save(item);
