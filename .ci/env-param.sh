d664c768c42e8d8196ed6e8716651f773e088e62
huydoan0212 <21130374@st.hcmuaf.edu.vn>
2025-08-15 14:00:25 +0700

    [tag] setup cicd

---
+++ .ci/env-param.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export project_name="meu-cms-auto"
+export image_name="registry.gitlab.com/meu-solutions/meu-cms-auto"
+export port_mapping="5001"
+export environment_name="staging"
+export mount_data_folder="/mnt/data"
+export release_version="1.0.0"
+export NODE_ENV=staging
+
+
+
+
+
