338aa19d46170bb118f852f048e920b3fe8f9321
huydoan0212 <21130374@st.hcmuaf.edu.vn>
2025-08-15 14:46:20 +0700

    [tag] setup cicd

--- .ci/env-param.sh
+++ .ci/env-param.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export project_name="meu-logistic-core"
-export image_name="registry.gitlab.com/meu-solutions/meu-logistic-core"
+export image_name="registry.gitlab.com/meusolutions/meu-logistic-core"
 export port_mapping="5001"
 export environment_name="staging"
 export mount_data_folder="/mnt/data"
