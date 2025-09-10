10603476b7b876a44ee4e2173dd27256c50554f5
Trần Trọng Nhân <heninduty@gmail.com>
2025-09-10 06:22:40 +0000

    Merge branch 'warrancybe' into 'develop'
    feat<BaoHanh>[api]: Lấy item để bảo hành
    
    See merge request dangdoan/meu-logistic-core!184
    
    (cherry picked from commit b908b5822b97c1878904c8f97de83207d562ce39)
    
    c024e603 feat<BaoHanh>[api]: Lấy item để bảo hành

---
+++ src/main/java/warehouse_management/com/warehouse_management/dto/inventory_item/response/InventoryItemWarrantyDto.java
@@ -0,0 +1,18 @@
+package warehouse_management.com.warehouse_management.dto.inventory_item.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+import java.time.LocalDateTime;
+
+@Data
+public class InventoryItemWarrantyDto {
+    private ObjectId id;
+    private String clientName;
+    private String model;
+    private String serialNumber;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime arrivalDate;
+}
