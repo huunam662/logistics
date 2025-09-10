ac9fa5a45fa53fc50d4ecdb0ce3b658ca0dd909c
quyhoaphantruong <quyhoaphantruong@gmail.com>
2025-09-10 20:52:26 +0700

    duplicate product code and commodity code

--- src/main/java/warehouse_management/com/warehouse_management/dto/delivery_order/response/DeliverySparePartDetailsDto.java
+++ src/main/java/warehouse_management/com/warehouse_management/dto/delivery_order/response/DeliverySparePartDetailsDto.java
@@ -1,6 +1,8 @@
 package warehouse_management.com.warehouse_management.dto.delivery_order.response;
 
 import lombok.Data;
+import org.bson.types.ObjectId;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -12,6 +14,7 @@ public class DeliverySparePartDetailsDto {
     private String poNumber;       // Số của Đơn đặt hàng (Purchase Order) – Bắt buộc
     private String contractNumber; // Số hợp đồng
     private Integer quantity;   // Số lượng hàng hóa
+    private ObjectId warehouseId;
     private String description;         // Mô tả
     private String notes;                  // Ghi chú chung – Không bắt buộc
     private Boolean isDelivered;    // Đã hoặc chưa giao
