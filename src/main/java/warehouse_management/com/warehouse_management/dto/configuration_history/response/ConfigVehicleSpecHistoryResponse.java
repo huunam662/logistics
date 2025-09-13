f39df418a32b21466bd27e48b5c1c0a414010d55
Nguyen_Huu_Nam <nguyen.hnam2024work@gmail.com>
2025-09-13 17:34:45 +0700

    configuration vehicle

---
+++ src/main/java/warehouse_management/com/warehouse_management/dto/configuration_history/response/ConfigVehicleSpecHistoryResponse.java
@@ -0,0 +1,46 @@
+package warehouse_management.com.warehouse_management.dto.configuration_history.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+@Data
+public class ConfigVehicleSpecHistoryResponse {
+
+    private ObjectId id; // _id – Khóa chính tự động tạo bởi MongoDB
+    private String inventoryType;   //InventoryType
+    private String productCode;    // Mã định danh của sản phẩm (đối với sản phẩm xe & phụ kiện, phụ tùng thuộc sản phẩm này) – Bắt buộc
+    private String serialNumber;   // Số seri – Có cho xe/phụ kiện
+    private String model;          // Model sản phẩm – Bắt buộc
+    private String status;         // Trạng thái hiện tại (IN_STOCK, IN_TRANSIT...) – Bắt buộc
+
+    private Specifications specificationsBase;
+
+    private List<ConfigurationHistoryResponse> configHistories;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Specifications {
+        //PK-KN
+        private Integer liftingCapacityKg;      // Sức nâng (kg)
+        private String chassisType;             // Loại khung nâng
+        private Integer liftingHeightMm;        // Độ cao nâng (mm)
+        //PK-BINHDIEN
+        private String batteryInfo;             // Thông tin bình điện
+        private String batterySpecification;    // Thông số bình điện
+
+        //PK-SAC
+        private String chargerSpecification;    // Thông số bộ sạc
+
+
+        private String engineType;              // Loại động cơ
+        private String forkDimensions;          // Thông số càng
+        private Integer valveCount;             // Số lượng van
+        private Boolean hasSideShift;           // Có side shift không
+        private String otherDetails;
+    }
+}
---
+++ src/main/java/warehouse_management/com/warehouse_management/dto/configuration_history/response/ConfigurationHistoryResponse.java
@@ -0,0 +1,31 @@
+package warehouse_management.com.warehouse_management.dto.configuration_history.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ConfigurationHistoryResponse {
+
+    private ObjectId id;
+
+    private ObjectId vehicleId;
+
+    private ObjectId componentOldId; // Id của bộ phận cũ
+    private String componentOldSerial;  // Serial của bộ phận mới
+
+    private ObjectId componentReplaceId;    // Id của bộ phận được thay thế
+    private String componentReplaceSerial;  // Serial của bộ phận được thay thế
+
+    private String componentType;    // Loại bộ phận cũ
+
+    private String configType;    // Kiểu cấu hình thay đổi
+
+    private String description;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+}
