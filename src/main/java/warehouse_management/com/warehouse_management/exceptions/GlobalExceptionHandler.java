75759c5681ca851b956c84176b349ace5c50ebd6
tuan <tuan>
2025-09-15 16:53:55 +0700

    [tag]staging

--- src/main/java/warehouse_management/com/warehouse_management/exceptions/GlobalExceptionHandler.java
+++ src/main/java/warehouse_management/com/warehouse_management/exceptions/GlobalExceptionHandler.java
@@ -198,6 +198,6 @@ public class GlobalExceptionHandler {
                 .body(ApiResponse.fail( "Unexpected server error."+ ex.getMessage()+ex.toString()));
     }
 
-//next 1 2 3 four 5 6 7 8 9 10 11 12 15 16
+//next 1 2 3 four 5 6 7 8 9 10 11 12 15 16 17
 }
 
