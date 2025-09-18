76cbfd48c3ba0d4f3c4b6bd8df6ce2226fa72cda
Nguyen_Huu_Nam <nguyen.hnam2024work@gmail.com>
2025-09-18 13:04:51 +0700

    validation input create product/spare part

--- src/main/java/warehouse_management/com/warehouse_management/repository/configuration_history/ConfigurationHistoryRepository.java
+++ src/main/java/warehouse_management/com/warehouse_management/repository/configuration_history/ConfigurationHistoryRepository.java
@@ -1,6 +1,7 @@
 package warehouse_management.com.warehouse_management.repository.configuration_history;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import warehouse_management.com.warehouse_management.model.ConfigurationHistory;
 
@@ -13,5 +14,11 @@ public interface ConfigurationHistoryRepository extends CustomConfigurationHisto
     // Lấy bản cấu hình mới nhất theo vehicleId
     Optional<ConfigurationHistory> findTopByVehicleIdOrderByCreatedAtDesc(ObjectId vehicleId);
 
+    @Aggregation(pipeline = {
+            "{$match: {vehicleId: ?0, deletedAt: null}}",
+            "{$lookup: {from: 'client', localField: 'vehicleId', foreignField: '_id', as: 'vehicle'}}",
+            "{$unwind: '$vehicle'}",
+            "{$match: {}}"
+    })
     List<ConfigurationHistory> findByVehicleIdOrderByCreatedAtDesc(ObjectId vehicleId);
 }
