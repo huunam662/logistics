73008cf04e8fc0d81e4e11263eb2a3fe3b36f449
Nguyen_Huu_Nam <nguyen.hnam2024work@gmail.com>
2025-09-19 10:31:26 +0700

    fix

--- src/main/java/warehouse_management/com/warehouse_management/app/WritePriceToDb.java
+++ src/main/java/warehouse_management/com/warehouse_management/app/WritePriceToDb.java
@@ -13,6 +13,11 @@ public class WritePriceToDb implements Converter<BigDecimal, Decimal128> {
     @Override
     public Decimal128 convert(BigDecimal source) {
         if (source == null) return null;
+
+        if (source.scale() == 0) {
+            return new Decimal128(source);
+        }
+
         return new Decimal128(source.setScale(2, RoundingMode.HALF_UP));
     }
 }
