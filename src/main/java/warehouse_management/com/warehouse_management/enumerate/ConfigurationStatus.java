71743309d5a3d76a76c0d3d2cd8992001a794e80
huunam66 <nguyen.hnam2024work@gmail.com>
2025-09-24 22:04:16 +0700

    update status configurations

--- src/main/java/warehouse_management/com/warehouse_management/enumerate/ConfigurationStatus.java
+++ src/main/java/warehouse_management/com/warehouse_management/enumerate/ConfigurationStatus.java
@@ -5,22 +5,24 @@ import lombok.Getter;
 @Getter
 public enum ConfigurationStatus {
 
-    PENDING("PENDING"),
-    REPAIRING("REPAIRING"),
-    COMPLETED("COMPLETED");
+    PENDING("PENDING", "Chờ xác nhận"),
+    REPAIRING("REPAIRING", "Đang sửa chữa"),
+    COMPLETED("COMPLETED", "Hoàn tất");
 
+    private final String id;
     private final String value;
 
-    ConfigurationStatus(String value){
+    ConfigurationStatus(String id, String value){
+        this.id = id;
         this.value = value;
     }
 
-    public static ConfigurationStatus fromValue(String value){
+    public static ConfigurationStatus fromId(String id){
 
-        if(value == null) return null;
+        if(id == null) return null;
 
         for (ConfigurationStatus status : ConfigurationStatus.values()){
-            if (status.value.equals(value)){
+            if (status.value.equals(id)){
                 return status;
             }
         }
